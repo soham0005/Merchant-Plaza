b2fde0848b3e48ab87ce5d5b228688273f4e4ac0
soham0005 <sohamadhyapak0005@gmail.com>
2023-06-26 22:18:30 +0530

    Frontend Setup, UI

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Login from './pages/Login/Login'
+import Register from './pages/Register/Register'
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
